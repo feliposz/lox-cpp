933c5ce3272fbf205685d27353f863f94c6da99a
feliposz <feliposz@gmail.com>
2021-03-22 04:42:21 -0300

    Visitor interface for AST (incomplete)

--- Expr.h
+++ Expr.h
@@ -1,26 +1,99 @@
 #pragma once
-class Expr
-{
-    // 
-}
 
-class Binary : public Expr
-{
-    // Expr left, Token operator, Expr right
-}
+#include "Token.h"
 
-class Grouping : public Expr
-{
-    // Expr expression
-}
+struct Binary;
+struct Grouping;
+struct Literal;
+struct Unary;
 
-class Literal : public Expr
+template <typename R>
+struct Visitor
 {
-    // Object value
-}
+    R visitBinaryExpr(Binary *expr);
+    R visitGroupingExpr(Grouping *expr);
+    R visitLiteralExpr(Literal *expr);
+    R visitUnaryExpr(Unary *expr);
+};
 
-class Unary : public Expr
+template <typename R>
+struct Expr
 {
-    // Token operator, Expr right
-}
+
+    Expr()
+    {
+    }
+
+    virtual R accept(Visitor<R> visitor);
+};
+
+template <typename R>
+struct Binary : public Expr<R>
+{
+    Expr *left;
+    Token *oper;
+    Expr *right;
+
+    Binary(Expr *left, Token *oper, Expr *right)
+    {
+        this->left = left;
+        this->oper = oper;
+        this->right = right;
+    }
+
+    override accept(Visitor<R> visitor)
+    {
+        return visitor.visitBinaryExpr(this);
+    }
+};
+
+template <typename R>
+struct Grouping : public Expr<R>
+{
+    Expr *expression;
+
+    Grouping(Expr *expression)
+    {
+        this->expression = expression;
+    }
+
+    override accept(Visitor<R> visitor)
+    {
+        return visitor.visitGroupingExpr(this);
+    }
+};
+
+template <typename R>
+struct Literal : public Expr<R>
+{
+    Object *value;
+
+    Literal(Object *value)
+    {
+        this->value = value;
+    }
+
+    override accept(Visitor<R> visitor)
+    {
+        return visitor.visitLiteralExpr(this);
+    }
+};
+
+template <typename R>
+struct Unary<R> : public Expr<R>
+{
+    Token *oper;
+    Expr *right;
+
+    Unary(Token *oper, Expr *right)
+    {
+        this->oper = oper;
+        this->right = right;
+    }
+
+    override accept(Visitor<R> visitor)
+    {
+        return visitor.visitUnaryExpr(this);
+    }
+};
 
--- lox-generate-ast/lox-generate-ast.cpp
+++ lox-generate-ast/lox-generate-ast.cpp
@@ -6,50 +6,143 @@
 
 #define EXIT_USAGE 64
 
-void defineType(std::ofstream &writer, const char *basename, std::string &className, std::string &fields)
-{
-    if (basename)
-    {
-        writer << "class " << className << " : public " << basename << std::endl;
-    }
-    else
-    {
-        writer << "class " << className << std::endl;
-    }
-    writer << "{" << std::endl;
-    writer << "    // " << fields << std::endl;
-    writer << "}" << std::endl << std::endl;
-}
-
 void trimString(std::string &str)
 {
     str.erase(0, str.find_first_not_of(" "));
     str.erase(str.find_last_not_of(" ") + 1);
 }
 
+void splitString(char splitChar, std::string &str, std::vector<std::string> &splitStr)
+{
+    std::stringstream ss(str);
+    std::string part;
+    while (std::getline(ss, part, splitChar))
+    {
+        trimString(part);
+        splitStr.push_back(part);
+    }
+}
+
+void defineType(std::ofstream &writer, const char *basename, std::string &className, std::string &fields)
+{
+    writer << "template <typename R>" << std::endl;
+    if (basename)
+    {
+        writer << "struct " << className << " : public " << basename << std::endl;
+    }
+    else
+    {
+        writer << "struct " << className << std::endl;
+    }
+        
+    std::vector<std::string> fieldVec;
+    splitString(',', fields, fieldVec);
+    
+    writer << "{" << std::endl;
+
+    // Members
+    for (auto &field : fieldVec)
+    {
+        std::vector<std::string> parts;
+        splitString(' ', field, parts);
+        writer << "    " << parts[0] << " *" << parts[1] << ";" << std::endl;
+    }
+
+    // Constructor parameters
+    writer << std::endl;
+    writer << "    " << className << "(";
+    bool first = true;
+    for (auto &field : fieldVec)
+    {
+        std::vector<std::string> parts;
+        splitString(' ', field, parts);
+        if (!first)
+        {
+            writer << ", ";
+        }
+        writer << parts[0] << " *" << parts[1];;
+        first = false;
+    }
+    writer << ")" << std::endl;
+
+    // Constructor body
+    writer << "    {" << std::endl;
+    for (auto &field : fieldVec)
+    {
+        std::vector<std::string> parts;
+        splitString(' ', field, parts);
+        writer << "        this->" << parts[1] << " = " << parts[1] << ";" << std::endl;
+    }
+    writer << "    }" << std::endl;
+
+    // Accept method
+    if (basename)
+    {
+        writer << std::endl;
+        writer << "    override accept(Visitor<R> visitor)" << std::endl;
+        writer << "    {" << std::endl;
+        writer << "        return visitor.visit" << className << basename << "(this);" << std::endl;
+        writer << "    }" << std::endl;
+    }
+    else
+    {
+        writer << std::endl;
+        writer << "    virtual R accept(Visitor<R> visitor);" << std::endl;
+    }
+
+    writer << "};" << std::endl << std::endl;
+}
+
+void defineVisitor(std::ofstream &writer, std::string &baseName, std::vector<std::string> &types)
+{
+    std::string lowerBaseName;
+    for (auto c : baseName)
+    {
+        lowerBaseName.append(1, tolower(c));
+    }
+
+    // forward declarations
+    for (auto &type : types)
+    {
+        std::vector<std::string> parts;
+        splitString(':', type, parts);
+        writer << "struct " << parts[0] << ";" << std::endl;
+    }
+
+    // "abstract" visitor
+    writer << std::endl;
+    writer << "template <typename R>" << std::endl;
+    writer << "struct Visitor" << std::endl;
+    writer << "{" << std::endl;
+    for (auto &type : types)
+    {
+        std::vector<std::string> parts;
+        splitString(':', type, parts);
+        writer << "    R visit" << parts[0] << baseName << "(" << parts[0] << " *" << lowerBaseName << ");" << std::endl;
+    }
+    writer << "};" << std::endl << std::endl;
+}
+
 void defineAst(const char *outputDir, const char *basename, std::vector<std::string> &types)
 {
-    std::ostrstream path; // TODO: change to ostringstream?
+    std::ostringstream path; // TODO: change to ostringstream?
     path << outputDir << "/" << basename << ".h" << std::ends;
-    char *pathStr = path.str();
-    std::ofstream writer(pathStr);
+    std::ofstream writer(path.str());
     if (!writer.bad())
     {
-        writer << "#pragma once" << std::endl;
-
+        writer << "#pragma once" << std::endl << std::endl;
+        writer << "#include \"Token.h\"" << std::endl << std::endl;
+        
         std::string baseClassName(basename);
         std::string baseFields;
+        defineVisitor(writer, baseClassName, types);
         defineType(writer, 0, baseClassName, baseFields);
 
         for (auto &type : types)
         {
-            std::stringstream ss(type);
-            std::string className, fields;
-            std::getline(ss, className, ':');
-            std::getline(ss, fields, ':');
-            trimString(className);
-            trimString(fields);
-            defineType(writer, basename, className, fields);
+            std::vector<std::string> parts;
+            splitString(':', type, parts);
+            defineType(writer, basename, parts[0], parts[1]);
         }
 
         writer.close();
@@ -58,7 +151,6 @@ void defineAst(const char *outputDir, const char *basename, std::vector<std::str
     {
         std::cerr << "Error opening " << path.str() << " for writing." << std::endl;
     }
-    path.freeze(false);
 }
 
 int main(int argc, char *argv[])
@@ -70,10 +162,10 @@ int main(int argc, char *argv[])
     }
 
     std::vector<std::string> types = {
-        "Binary   : Expr left, Token operator, Expr right",
+        "Binary   : Expr left, Token oper, Expr right",
         "Grouping : Expr expression",
         "Literal  : Object value",
-        "Unary    : Token operator, Expr right"
+        "Unary    : Token oper, Expr right"
     };
     const char *outputDir = argv[1];
     defineAst(outputDir, "Expr", types);
