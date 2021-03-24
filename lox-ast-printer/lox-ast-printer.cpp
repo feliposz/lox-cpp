07a8f76d0a36277dd01f785e7a1ea1da98fb8772
feliposz <feliposz@gmail.com>
2021-03-24 04:02:17 -0300

    Extracted AstPrinter to separate header

--- lox-ast-printer/lox-ast-printer.cpp
+++ lox-ast-printer/lox-ast-printer.cpp
@@ -1,51 +1,4 @@
-#include <iostream>
-#include <string>
-#include "../Expr.h"
-
-std::string visit(Expr *expr);
-
-std::string parenthesize(std::string name, Expr *expr1 = 0, Expr *expr2 = 0, Expr *expr3 = 0)
-{
-    std::stringstream ss;
-    ss << "(" << name;
-    if (expr1) ss << " " << visit(expr1);
-    if (expr2) ss << " " << visit(expr2);
-    if (expr3) ss << " " << visit(expr3);
-    ss << ")";
-    return ss.str();
-}
-
-std::string visitBinary(Binary *expr)
-{
-    return parenthesize(expr->oper->lexeme, expr->left, expr->right);
-}
-
-std::string visitGrouping(Grouping *expr)
-{
-    return parenthesize("group", expr->expression);
-}
-
-std::string visitLiteral(Literal *expr)
-{
-    return expr->value->str();
-}
-
-std::string visitUnary(Unary *expr)
-{
-    return parenthesize(expr->oper->lexeme, expr->right);
-}
-
-std::string visit(Expr *expr)
-{
-    switch (expr->type)
-    {
-        case ExprType_Binary: return visitBinary((Binary *)expr);
-        case ExprType_Grouping: return visitGrouping((Grouping *)expr);
-        case ExprType_Literal: return visitLiteral((Literal *)expr);
-        case ExprType_Unary: return visitUnary((Unary *)expr);
-    }
-    return "<<<INVALID EXPR TYPE>>>";
-}
+#include "../AstPrinter.h"
 
 int main(int argc, char *argv[])
 {
@@ -60,7 +13,7 @@ int main(int argc, char *argv[])
     Grouping *grp = new Grouping(l45);
     Expr *expr = new Binary(um123, star, grp);
 
-    std::cout << visit(expr) << std::endl;
+    AstPrinter::print(expr);
 
     // TODO: leaking memory... (use unique_ptr???)
 
